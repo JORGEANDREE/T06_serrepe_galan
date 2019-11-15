a3da7110f68a1fa002f5086a976fa1f81bfe9c39
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 14:15:22 -0500

    se agrego el 1er ejercicio de condicional simple

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+import os
+#declarar valores
+nota1,nota2,nota3,promedio=0.0,0.0,0.0,0.0
+
+#pedir variables via argumentos
+nota1=float(os.sys.argv[1])
+nota2=float(os.sys.argv[2])
+nota3=float(os.sys.argv[3])
+promedio=(nota1+nota2+nota3)
+
+#validacion
+aprobado=(promedio>10.5)
+
+#condicional simple
+if(aprobado==True):
+    print("APROBADO")
+#fin_if
