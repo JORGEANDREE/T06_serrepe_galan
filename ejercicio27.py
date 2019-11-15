d5fc544486b3c7649ba93da323dafb319bd5a867
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 15:55:47 -0500

    se agrego el 7mo ejercicio de condicional multiple

---
+++ ejercicio27.py
@@ -0,0 +1,31 @@
+import os
+#declarar variables
+alumno,trabajos_de_programacion,trabajos_de_analisis,trabajos_de_matematica,tareas_excesivas,total_tareas="",0,0,0,0,0
+
+#pedir las variables via argumentos
+alumno=os.sys.argv[1]
+trabajos_de_programacion=int(os.sys.argv[2])
+trabajos_de_analisis=int(os.sys.argv[3])
+trabajos_de_matematica=int(os.sys.argv[4])
+total_tareas=trabajos_de_matematica+trabajos_de_analisis+trabajos_de_programacion
+
+#impresion
+print("#################################")
+print("##############TAREAS#############")
+print("# alumno:",alumno)
+print("# Trabajos de programacion:",trabajos_de_programacion)
+print("# Trabajos de analisis:",trabajos_de_analisis)
+print("# Trabajos de matematica:",trabajos_de_matematica)
+print("# Total de Tareas:",total_tareas)
+print("#################################")
+
+#condicional multiple
+if(total_tareas<4):
+    print("!pocas tareas! ")
+#fin_if
+if(total_tareas>5 and total_tareas<10):
+    print("!muchas tareas! ")
+#fin_if
+if(total_tareas>11 and total_tareas<20):
+    print("!excesivas tareas! ")
+#fin_if
