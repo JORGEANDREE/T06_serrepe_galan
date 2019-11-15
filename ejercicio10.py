ab068bc2c67893f6029c9ccc409e6885ce35dbf8
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 14:26:50 -0500

    se agrego el 10mo ejercicio de condicional simple

---
+++ ejercicio10.py
@@ -0,0 +1,25 @@
+import os
+#declarar variables
+perros,gatos,loros,total_granja=0,0,0,0
+
+#pedir las variables via argumentos
+perros=int(os.sys.argv[1])
+gatos=int(os.sys.argv[2])
+loros=int(os.sys.argv[3])
+total_granja=perros+gatos+loros
+#impresion
+print("#################################")
+print("###############GRANJA############")
+print("# nro-perros",perros)
+print("# nro-gatos:",gatos)
+print("# nro-loros:",loros)
+print("# total de animales:",total_granja)
+print("#################################")
+
+#validacion
+exceso_de_animales=(total_granja>50)
+
+#condicional simple
+if(exceso_de_animales==True):
+    print("GRANJA LLENA")
+#fin_if
