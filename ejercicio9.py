5083008a4faefb79af71ac03b551933382d7861e
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 14:25:46 -0500

    se agrego el 9no ejercicio de condicional simple

---
+++ ejercicio9.py
@@ -0,0 +1,26 @@
+import os
+#declarar variables
+persona,litros_de_agua,comida,mochila_supervivencia="",0,0,0
+
+#pedir las variables via argumentos
+persona=os.sys.argv[1]
+litros_de_agua=int(os.sys.argv[2])
+comida=int(os.sys.argv[3])
+mochila_supervivencia=trabajos_de_matematica+trabajos_de_analisis+trabajos_de_programacion
+
+#validacion
+mochila_pesada=(mochila_supervivencia>40)
+
+#impresion
+print("#################################")
+print("##############Mochila#############")
+print("# persona:",persona)
+print("# litros de agua:",litros_de_agua)
+print("# comida:",comida)
+print("# mochila de supervivencia:",mochila_supervivencia,"Kg")
+print("#################################")
+
+#condicional simple
+if(mochila_pesada==True):
+    print("mochila pesada !sobrepeso! ")
+#fin_if
