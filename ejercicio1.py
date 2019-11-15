b1c217f5732177b22d76006cf6548a021a13fc58
JORGEANDREE <57020796+JORGEANDREE@users.noreply.github.com>
2019-11-15 14:51:03 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,7 +6,7 @@ nota1,nota2,nota3,promedio=0.0,0.0,0.0,0.0
 nota1=float(os.sys.argv[1])
 nota2=float(os.sys.argv[2])
 nota3=float(os.sys.argv[3])
-promedio=(nota1+nota2+nota3)
+promedio=(nota1+nota2+nota3)/3
 
 #validacion
 aprobado=(promedio>10.5)
