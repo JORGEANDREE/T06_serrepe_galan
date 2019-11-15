f45eccc881d5d712bbfa937f526dd814d9227c9b
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 14:56:19 -0500

    se agrego el 2do ejercicio de condicional doble

---
+++ ejercicio12.py
@@ -0,0 +1,26 @@
+import os
+#declarar variables
+dinero,pu_ticket,nro_tickes=0.0,0.0,0.0
+
+#pedir las variables via argumentos
+dinero=float(os.sys.argv[1])
+pu_ticket=float(os.sys.argv[2])
+nro_tickes=dinero//pu_ticket
+#impresion
+print("#################################")
+print("###############JUEGOS############")
+print("# dinero",dinero)
+print("# precio unitario del ticket:",pu_ticket)
+print("# NRO ticket:",nro_tickes)
+print("#################################")
+
+#validacion
+vale=(nro_tickes>100.0)
+
+#condicional simple
+if(vale==True):
+    print("GANASTE 1 VALE PARA UN JUEGO MAS")
+#fin_if
+else:
+    print("SIGUE INTENTANDO")
+#fin_else
