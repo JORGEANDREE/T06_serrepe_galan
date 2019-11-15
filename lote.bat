4a7dbc0692962277259b1fef0821803808f7ba34
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 18:59:32 -0500

    se agrego el 2do ejercicio de condicional doble

---
+++ lote.bat
@@ -0,0 +1,66 @@
+python serrepe/ejercicio1.py 11.3 12.4 15.6
+python serrepe/ejercicio1.py 13.4 11.4 16.7
+python serrepe/ejercicio1.py 17.3 18.9 20.0
+python serrepe/ejercicio2.py 200.5 1.3
+python serrepe/ejercicio2.py 400.0 2.0
+python serrepe/ejercicio2.py 500.5 3.3
+python serrepe/ejercicio3.py "jorgeandree@g.mail.com" "mundo" 30 80
+python serrepe/ejercicio3.py "serrepe@g.mail.com" "hola" 10 20
+python serrepe/ejercicio3.py "pycharm@g.mail.com" "feliz" 50 70
+python serrepe/ejercicio4.py "jorgeandree" 3 1000
+python serrepe/ejercicio4.py "raul" 7 2000
+python serrepe/ejercicio4.py "jose" 12 100
+python serrepe/ejercicio5.py  12 13 14 2 4 5
+python serrepe/ejercicio5.py  10 3 17 2 3 5
+python serrepe/ejercicio5.py  10 3 4 2 4 5
+python serrepe/ejercicio6.py "jorgeandree" 200.4 4
+python serrepe/ejercicio6.py "rosafernandez" 30.5 5
+python serrepe/ejercicio6.py "jefrry" 10.4 6
+python serrepe/ejercicio7.py "fiorella" 3 4 5
+python serrepe/ejercicio7.py "dina" 4 6 7
+python serrepe/ejercicio7.py "cristian" 3 2 1
+python serrepe/ejercicio8.py "tania" "fredyy" 24 31 10 2 34 4
+python serrepe/ejercicio8.py "jorge" "kasandra" 20 11 12 10 12 5
+python serrepe/ejercicio8.py "milagros" "sandra" 12 1 10 3 4 5
+python serrepe/ejercicio9.py "bonyy"  13 45
+python serrepe/ejercicio9.py "sheila"  15 30
+python serrepe/ejercicio9.py "yadhira"  10 25
+python serrepe/ejercicio10.py 20  23 45
+python serrepe/ejercicio10.py 10  13 34
+python serrepe/ejercicio10.py 12  13 15
+python serrepe/ejercicio11.py 11.3 13.4 12.4
+python serrepe/ejercicio11.py 20.0 12.2 0.9
+python serrepe/ejercicio11.py 0.3. 10.2 5.9
+python serrepe/ejercicio12.py 222.3 1.3
+python serrepe/ejercicio12.py 400.23 2.3
+python serrepe/ejercicio12.py 100.0 2.3
+python serrepe/ejercicio13.py "jorgeandree@g.mail.com" "mundo" 30 90
+python serrepe/ejercicio13.py "andree@g.mail.com" "jorge" 10 30
+python serrepe/ejercicio13.py "rosa@g.mail.com" "1234" 5 70
+python serrepe/ejercicio14.py "jorge" 4 1000
+python serrepe/ejercicio14.py "xiomara" 1 1000
+python serrepe/ejercicio14.py "jose" 2 120
+python serrepe/ejercicio15.py  1 13 14 2 4 8
+python serrepe/ejercicio15.py  10 3 12 1 3 4
+python serrepe/ejercicio15.py  1 3 4 4 4 5
+python serrepe/ejercicio16.py "jota" 202.4 5
+python serrepe/ejercicio16.py "klibre" 30.5 2
+python serrepe/ejercicio16.py "sanchez" 11.4 3
+python serrepe/ejercicio17.py "zuly" 1 4 6
+python serrepe/ejercicio17.py "tatiana" 4 7 9
+python serrepe/ejercicio17.py "brando" 1 2 2
+python serrepe/ejercicio18.py "carlos" "franz" 14 1 10 2 4 4
+python serrepe/ejercicio18.py "marcos" "gabriel" 22 13 10 10 10 5
+python serrepe/ejercicio18.py "sonia" "farid" 1 1 10 2 4 5
+python serrepe/ejercicio19.py "daniel"  18 49
+python serrepe/ejercicio19.py "daniela"  4 30
+python serrepe/ejercicio19.py "gilmer"  10 25
+python serrepe/ejercicio20.py 2  23 85
+python serrepe/ejercicio20.py 14  3 14
+python serrepe/ejercicio20.py 15  3 15
+python serrepe/ejercicio21.py 14.3 13.4 15.0
+python serrepe/ejercicio21.py 17. 19.4 18.7
+python serrepe/ejercicio21.py 16.0 16.9 16.0
+python serrepe/ejercicio22.py 200.5 1.3
+python serrepe/ejercicio22.py 400.0 2.0
+python serrepe/ejercicio22.py 500.5 3.3
