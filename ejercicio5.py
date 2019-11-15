9739e2092427fe9a6a0caa81b3f846c043f8b280
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 14:21:46 -0500

    se agrego el 5to ejercicio de condicional simple

---
+++ ejercicio5.py
@@ -0,0 +1,28 @@
+import os
+#declarar variables
+jabones,shampo,acondicionador,pu_jabones,pu_shampo,pu_acondicionador,total=0,0,0,0,0,0,0
+
+#pedir las variables via argumentos
+jabones=int(os.sys.argv[1])
+shampo=int(os.sys.argv[2])
+acondicionador=int(os.sys.argv[3])
+pu_acondicionador=int(os.sys.argv[4])
+pu_jabones=int(os.sys.argv[5])
+pu_shampo=int(os.sys.argv[6])
+total=(jabones*pu_jabones)+(shampo*pu_shampo)+(acondicionador*pu_acondicionador)
+#impresion
+print("#################################")
+print("###############COMPRAS###########")
+print("# jabones",jabones,"  p.u:",pu_jabones)
+print("# shampo:",shampo,"   p.u:",pu_shampo)
+print("# acondicinador:",acondicionador, "  pu:",pu_acondicionador)
+print("# Total:",total,"$")
+print("#################################")
+
+#validacion
+bonus=(total>100.0)
+
+#condicional simple
+if(bonus==True):
+    print("GANASTE UN SHAMPO!")
+#fin_if
