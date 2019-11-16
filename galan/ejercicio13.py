4a9d0f30ce8b659be50ec16e0b6f7dc310cc7526
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 20:55:29 -0500

    se agrego el 3er ejercicio de condicional doble

---
+++ galan/ejercicio13.py
@@ -0,0 +1,29 @@
+import os
+#declarar variables
+computadoras,laptops,celulares,total=0,0,0,0
+
+#pedir las variables via argumentos
+computadoras=int(os.sys.argv[1])
+laptops=int(os.sys.argv[2])
+celulares=int(os.sys.argv[3])
+total=computadoras+laptops+celulares
+
+#impresion
+print("#################################")
+print("###############TIENDA ELECTRONICA############")
+print("# N-computadoras",computadoras)
+print("# N-laptops:",laptops)
+print("# N-celulares:",celulares)
+print("# total:",total)
+print("#################################")
+
+#validacion
+ganaste=(total>100)
+
+#condicional doble
+if(ganaste==True):
+    print("GANASTE UN CELULAR ULTIMO MODELO")
+#fin_if
+else:
+    print("GANASTE UN USB")
+#fin_else
