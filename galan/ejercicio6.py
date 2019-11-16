3cd5307276c9f800f04565bc55f2875c5ebd1194
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 20:25:33 -0500

    se agrego el 6to ejercicio de condicional simple

---
+++ galan/ejercicio6.py
@@ -0,0 +1,23 @@
+import os
+#declarar variables
+#calcular el peso
+peso,masa,gravedad=0.0,0.0,0.0
+
+#pedir las variables via argumentos
+masa=float(os.sys.argv[1])
+gravedad=float(os.sys.argv[2])
+peso=masa*gravedad
+
+#mostrar valores
+print("la masa es:",masa)
+print("la gravedad es:",gravedad)
+print("el peso es:",peso)
+
+#validacion
+peso_maximo=(peso>80)
+
+#condicional simple
+if(peso_maximo==True):
+    print("EL PESO ES MAXIMO")
+#fin_if
+
