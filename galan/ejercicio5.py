59723f05f3bff794b01b1a2adf5f308ac55caa6d
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 20:10:59 -0500

    se agrego el 5to ejercicio de condicional simple

---
+++ galan/ejercicio5.py
@@ -0,0 +1,24 @@
+import os
+#declarar variables (area de un rectangulo)
+base,altura,area=0.0,0.0,0.0
+
+#pedir las variables via argumentos
+base=float(os.sys.argv[1])
+altura=float(os.sys.argv[2])
+area=base*area
+
+#impresion
+print("#################################")
+print("###############AREA##############")
+print("# Base",base)
+print("# altura:",altura)
+print("# area:",area)
+print("#################################")
+
+#validacion
+maxima=(area>70)
+
+#condicional simple
+if(maxima==True):
+    print("AREA MAXIMA")
+#fin_if
