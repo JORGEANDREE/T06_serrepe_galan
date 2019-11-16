c70afcf837178619a29bec9794793cf5d9e3a26b
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-15 21:01:32 -0500

    se agrego el 9no ejercicio de condicional doble

---
+++ galan/ejercicio19.py
@@ -0,0 +1,28 @@
+import os
+#declarar variables
+residuos_plasticos,residuos_de_vidrio,residuos_papel,reciclados=0,0,0,0
+
+#pedir las variables via argumentos
+residuos_plasticos=int(os.sys.argv[1])
+residuos_de_vidrio=int(os.sys.argv[2])
+residuos_papel=int(os.sys.argv[3])
+reciclados=residuos_papel+residuos_de_vidrio+residuos_plasticos
+
+#impresion
+print("#################################")
+print("###############RECICLADORA############")
+print("# residuos platicos",residuos_plasticos)
+print("# residuos de vidrio:",residuos_de_vidrio)
+print("# residuos de papel:",edad_hijo)
+print("# total reciclados:",reciclados)
+print("#################################")
+
+#validacion
+total=(reciclados>100)
+
+#condicional simple
+if(superado==True):
+    print("RECICLA SALVA EL MUNDO")
+#fin_if
+else:
+    print("RECICLA MAS Y SALVARAS EL PLANETA")
